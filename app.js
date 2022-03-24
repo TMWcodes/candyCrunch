7bdeae42ba32831c50bcdd7b6f3063e1d0995ef1
exit <exit>
2022-03-24 00:24:53 +0000

    random colours

--- app.js
+++ app.js
@@ -3,15 +3,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const width = 8
     const squares = []
 
+    const candyColors = [
+        'red',
+        'yellow',
+        'orange',
+        'purple',
+        'green',
+        'blue'
+    ]
     //create board
     function createBoard() {
         for (let i = 0; i < width*width; i++) {
             const square = document.createElement('div')
+            let randomColor = Math.floor(Math.random() * candyColors.length)
+            square.style.backgroundColor = candyColors[randomColor]
             grid.appendChild(square)
             squares.push(square)
         }
     }
     createBoard()
 
-    
+
 })
